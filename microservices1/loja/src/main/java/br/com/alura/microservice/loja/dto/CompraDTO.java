8e8f7cd5e0306d9184ebd76bc28b0b3c0dd852c7
fabiosouza <fabiosouza.web@gmail.com>
2021-10-08 19:53:49 -0300

    Estrutura base e iniciando implementação do servico de compra

---
+++ microservices1/loja/src/main/java/br/com/alura/microservice/loja/dto/CompraDTO.java
@@ -0,0 +1,13 @@
+package br.com.alura.microservice.loja.dto;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class CompraDTO {
+
+	private List<ItemDaCompraDTO> itens;
+	
+	private EnderecoDTO endereco;
+}
---
+++ microservices1/loja/src/main/java/br/com/alura/microservice/loja/dto/ItemDaCompraDTO.java
@@ -0,0 +1,11 @@
+package br.com.alura.microservice.loja.dto;
+
+import lombok.Data;
+
+@Data
+public class ItemDaCompraDTO {
+
+	private long id;
+	
+	private int quantidade;
+}
