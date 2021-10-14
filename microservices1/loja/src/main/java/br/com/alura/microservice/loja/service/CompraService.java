264f114e82e9f32dbbfff1f8449b70480fabb6cf
fabiosouza <fabiosouza.web@gmail.com>
2021-10-14 16:19:19 -0300

    Tratamento de erros com armazenamento de estado

--- microservices1/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ microservices1/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import java.time.LocalDate;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,24 +11,24 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.dto.InfoEntregaDTO;
 import br.com.alura.microservice.loja.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.dto.VoucherDTO;
 import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.model.CompraState;
 import br.com.alura.microservice.loja.repository.CompraRepository;
 
 @Service
 public class CompraService {
-
-//	@Autowired
-//	private RestTemplate client;
-	
-//	@Autowired
-//	private DiscoveryClient eurekaClient;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
 	
 	@Autowired
 	private FornecedorClient fornecedorClient;
+	
+	@Autowired
+	private TransportadorClient transportadorClient;
 	
 	@Autowired
 	private CompraRepository repository;
@@ -40,41 +42,47 @@ public class CompraService {
 			threadPoolKey = "realizaCompraThreadPool") 
 	public Compra realizaCompra(CompraDTO compra) {
 		
+		Compra compraSalva = new Compra();
+		compraSalva.setState(CompraState.RECEBIDO);
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		repository.save(compraSalva);
+		compra.setCompraId(compraSalva.getId());
+				
 		String estado = compra.getEndereco().getEstado();
-		
 		LOG.info("Buscando informações do fornecedor de {}", estado);
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
 		
 		LOG.info("Realizando um pedido", estado);
 		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
-		
-		System.out.println(info.getEndereco());
-		
-		Compra compraSalva = new Compra();
+		compraSalva.setState(CompraState.PEDIDO_REALIZADO);
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
-		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		repository.save(compraSalva);
 		
+		InfoEntregaDTO entregaDto = new InfoEntregaDTO();
+		entregaDto.setPedidoId(pedido.getId());
+		entregaDto.setDataParaEntrega(LocalDate.now().plusDays(pedido.getTempoDePreparo()));
+		entregaDto.setEnderecoOrigem(info.getEndereco());
+		entregaDto.setEnderecoDestino(compra.getEndereco().toString());
+		VoucherDTO voucher = transportadorClient.reservaEntrega(entregaDto);
+		compraSalva.setState(CompraState.RESERVA_ENTREGA_REALIZADA);
+		compraSalva.setDataParaEntrega(voucher.getPrevisaoParaEntrega());
+		compraSalva.setVoucher(voucher.getNumero());
 		repository.save(compraSalva);
 		
 		return compraSalva;
-		
-//		REQUISIÇÃO SEM FEIGN
-//		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://FORNECEDOR/info/" + compra.getEndereco().getEstado(), 
-//				HttpMethod.GET, 
-//				null, 
-//				InfoFornecedorDTO.class);
-//		
-//		eurekaClient.getInstances("fornecedor").stream()
-//		.forEach(fornecedor -> {
-//			System.out.println("localhost:" + fornecedor.getPort());
-//		});
 	}
 	
 	// Método chamado quando realizaCompra() falhar
 	public Compra realizaCompraFallback(CompraDTO compra) {
+		
 		Compra compraFallback = new Compra();
 		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
-		return compraFallback;
+		
+		if(compra.getCompraId() == null) {
+			return compraFallback;
+		}
+		
+		return repository.findById(compra.getCompraId()).orElse(compraFallback);		
 	}
 }
